fc3f2ee58b470dba4507241f7cb2ef9581b29edc
inch <inch@localhost.localdomain>
2016-04-23 00:17:30 -0700

    ada

--- main.c
+++ main.c
@@ -2,22 +2,65 @@
 
 import re
 import urllib
+import urllib2
+hurl='http://175game.com/'
+		
+class strack(object):
+	def init(self):
+		self.point=-1
+		self.url={}
+		self.allurl={}
+	def pop(self):
+		if self.point >= 0:
+			var = self.url[self.point]
+		        self.point = self.point - 1
+			return var 
+		return None
+	def push(self,var):
+		if self.allurl.get(var) == None:
+			self.point = self.point + 1
+			self.url[self.point] = var		
+			self.allurl[var] = 1
 
 def GetPage(url):
-	page = urllib.urlopen(url)
+	try:
+		page = urllib2.urlopen(url)
+	except	urllib2.URLError,e:
+		return 
 	content = page.read()
 	return content
 
-def GetResource(hurl,content):
-	reg=r'src="(.*.[jpg,png])"'
-	comreg=re.compile(reg)
-	ResourceUrlList=re.findall(comreg,content)
-	print ResourceUrlList
-	count=0
-	for oneurl in ResourceUrlList:
-		oneurl=hurl+oneurl
-		urllib.urlretrieve(oneurl,'%d.img' % count)
-		count+=1	
+def main(targeturl):
+	surl=strack()
+	surl.init()
+	surl.push(targeturl)
 
-url='http://175game.com'
-GetResource(url,GetPage(url))	
+	regsrc=r'<img src=["]/(.*?.[jpg,png])["]'
+	regsrcname=r'.*/(.*.[jpg,png])'
+	reglink=r'<a href="/(.*?)".*>'
+
+	comregsrc=re.compile(regsrc)
+	comregsrcname=re.compile(regsrcname)
+	comreglink=re.compile(reglink)
+	
+	url=surl.pop()
+	ResourceUrlList = []
+	LinkList = []
+	while url!=None:
+		content=GetPage(url)
+		if content != None:
+			ResourceUrlList=re.findall(comregsrc,content)
+			LinkList=re.findall(comreglink,content)
+		for oneurl in ResourceUrlList:
+			filename=re.findall(comregsrcname,oneurl)
+			oneurl=hurl+oneurl
+			print oneurl
+			u=urllib.urlopen(oneurl)
+			data=u.read()
+			f=open(filename[0],'wb')
+			f.write(data)
+			f.close()
+		for onelink in LinkList:
+			surl.push(hurl+onelink)	
+		url=surl.pop()
+main(hurl)	
